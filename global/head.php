ee2f7d98950b840d12ba6a0b7c9ebfc33dec8e2f
Yaskovich Dmitry <kadavr95@outlook.com>
2016-08-23 22:16:00 +0300

    version 2.0.6.0
    Google Analytics modified to Google Tag Manager via php
    added ads to the bottom of the pages

--- global/head.php
+++ global/head.php
@@ -1,15 +1,4 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
 <link type="text/css" rel="stylesheet" href="/global/site_files/stylesheet.css">
 <script type="text/javascript" src="/global/site_files/javascript.js" async></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-82875223-1', 'auto');
-  ga('require', 'linkid');
-  ga('send', 'pageview');
-
-</script>
 <link rel ="shortcut icon" href="/global/site_files/favicon.ico">
