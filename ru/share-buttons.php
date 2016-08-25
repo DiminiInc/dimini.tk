921658f135039426ea215e5fe96da05ec5d70e23
Yaskovich Dmitry <kadavr95@outlook.com>
2016-08-26 00:15:00 +0300

    version 2.1.1.0
    localization and bug fixes

--- ru/share-buttons.php
+++ ru/share-buttons.php
@@ -1,5 +1,5 @@
 <div class="share-buttons">
-	<span>Share: </span>
+	<span>Поделиться: </span>
 	<a href="javascript:void(0)" onclick="sharePage('facebook');" class="nopadding-link"><img src="/global/site_files/facebook_logo.png"/></a> 
 	<a href="javascript:void(0)" onclick="sharePage('twitter');" class="nopadding-link"><img src="/global/site_files/twitter_logo.png"/></a> 
 	<a href="javascript:void(0)" onclick="sharePage('vk');" class="nopadding-link"><img src="/global/site_files/vk_logo.png"/></a> 
