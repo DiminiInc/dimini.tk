d532239a3b324b31648b19ce0e04686d951621aa
Yaskovich Dmitry <kadavr95@outlook.com>
2016-08-26 00:20:00 +0300

    version 2.1.1.1
    bugs fixes

--- ru/share-buttons.php
+++ ru/share-buttons.php
@@ -4,5 +4,5 @@
 	<a href="javascript:void(0)" onclick="sharePage('twitter');" class="nopadding-link"><img src="/global/site_files/twitter_logo.png"/></a> 
 	<a href="javascript:void(0)" onclick="sharePage('vk');" class="nopadding-link"><img src="/global/site_files/vk_logo.png"/></a> 
 	<a href="javascript:void(0)" onclick="sharePage('googleplus');" class="nopadding-link"><img src="/global/site_files/googleplus_logo.png"/></a> 
-	<a href="javascript:void(0)" onclick="sharePage('ok');" class="nopadding-link"><img src="/global/site_files/instagram_logo.png"/></a> 
+	<a href="javascript:void(0)" onclick="sharePage('ok');" class="nopadding-link"><img src="/global/site_files/ok_logo.png"/></a> 
 </div>
