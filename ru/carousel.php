7c3da44a59f5fe27f998b3160a04f712fcb59077
Yaskovich Dmitry <kadavr95@outlook.com>
2017-01-07 00:30:00 +0300

    version 2.3.2.0
    added recent software to carousel

--- ru/carousel.php
+++ ru/carousel.php
@@ -1,6 +1,8 @@
 <div class="carousel-wrapper" style="height: 400px;">
   <span class="hidden-target" id="browsers-comparison"></span>
   <span class="hidden-target" id="snake"></span>
+  <span class="hidden-target" id="battleship"></span>
+  <span class="hidden-target" id="game-of-life"></span>
   <span class="hidden-target" id="dimini-search"></span>
   <div class="carousel-item item-1" style="background-color: #ffffff; background-image: url(/global/site_files/browsers-carousel.png); background-size: 100%; background-position:center;">
     <a class="pagelink" href="/ru/testdrive/browsers">
@@ -18,12 +20,28 @@
     <a class="arrow arrow-prev" href="#browsers-comparison"></a>
     <a class="arrow arrow-next" href="#dimini-search"></a>
   </div>
-  <div class="carousel-item item-3" style="background-color: #ffffff; background-image: url(/global/site_files/disearch_logo.png); background-size: auto; background-repeat: no-repeat; background-position:center;">
+  <div class="carousel-item item-3" style="background-color: #ffffff; background-image: url(/global/site_files/battleship-logo.png); background-size: auto; background-repeat: no-repeat; background-position:center;">
+    <a class="pagelink" href="/ru/software/battleship">
+      <h2>Морской бой</h2>
+      <p>Популярная игра "Морской бой"</p>
+    </a>
+    <a class="arrow arrow-prev" href="#snake"></a>
+    <a class="arrow arrow-next" href="#game-of-life"></a>
+  </div>
+  <div class="carousel-item item-4" style="background-color: #ffffff; background-image: url(/global/site_files/game-of-life-logo.png); background-size: auto; background-repeat: no-repeat; background-position:center;">
+    <a class="pagelink" href="/ru/software/game-of-life">
+      <h2>Игра "Жизнь"</h2>
+      <p>Клеточный автомат Джона Конвея</p>
+    </a>
+    <a class="arrow arrow-prev" href="#battleship"></a>
+    <a class="arrow arrow-next" href="#dimini-search"></a>
+  </div>
+  <div class="carousel-item item-5" style="background-color: #ffffff; background-image: url(/global/site_files/disearch_logo.png); background-size: auto; background-repeat: no-repeat; background-position:center;">
     <a class="pagelink" href="/ru/software/disearch">
       <h2>Dimini Search</h2>
       <p>Программа для быстрого поиска в различных поисковых системах</p>
     </a>
-    <a class="arrow arrow-prev" href="#snake"></a>
+    <a class="arrow arrow-prev" href="#game-of-life"></a>
     <a class="arrow arrow-next" href="#browsers-comparison"></a>
   </div>
 </div>
